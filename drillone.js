49072191a727e898a60953c102ebb6f2e2e9eb70
Vendy Prum <vendy@thinkful.com>
2020-02-11 12:12:11 -0800

    update selfreference

--- drillone.js
+++ drillone.js
@@ -1,13 +1,29 @@
-function createMyObject() {
-    return obj = {
-    foo: 'bar',
-    anwserToUniverse: 42,
-    "olly olly": 'oxen free',
-    sayHello: function() {
-        return 'Hello'
-    }
-  }
-    // return obj
+'use strict';
+
+// function createMyObject() {
+//   return obj = {
+//     foo: 'bar',
+//     anwserToUniverse: 42,
+//     "olly olly": 'oxen free',
+//     sayHello: function() {
+//       return 'Hello';
+//     }
+//   };
+// }
+
+//self reference
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  
+  return person;
 }
 
-// console.log(createMyObject())
+
