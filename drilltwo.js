842961616703fab9ad7bc578711b050453a16c2a
Yghaddar <58191171+yghaddar@users.noreply.github.com>
2020-02-11 16:41:31 -0500

    i dont know how to spell function

--- drilltwo.js
+++ drilltwo.js
@@ -2,32 +2,66 @@
 
 //make student reports 
 
-function makeStudentReport(data){
-// let arr = [] 
-// for (let key in data){ 
-//   arr.push(`${data.name}: ${data.grade}`) 
-// } 
-//   return arr 
+// function makeStudentReport(data){
+// // let arr = [] 
+// // for (let key in data){ 
+// //   arr.push(`${data.name}: ${data.grade}`) 
+// // } 
+// //   return arr 
+// // }
+
+
+
+//   let array = [];
+
+//   for(let i = 0; i < data.length; i++){
+//     let nameValue = data[i].name;
+//     let nameGrade = data[i].grade;
+//     array.push(`[${nameValue}]: [${nameGrade}]`); 
+
+//   }
+//   return array;
 // }
 
+// const testData = [
+//   { name: 'Jane Doe', grade: 'A' },
+//   { name: 'John Dough', grade: 'B' },
+//   { name: 'Jill Do', grade: 'A' },
+// ];
 
 
-  let array = [];
+// console.log(makeStudentReport(testData));
 
-  for(let i = 0; i < data.length; i++){
-    let nameValue = data[i].name;
-    let nameGrade = data[i].grade;
-    array.push(`[${nameValue}]: [${nameGrade}]`); 
 
-  }
-  return array;
-}
 
-const testData = [
-  { name: 'Jane Doe', grade: 'A' },
-  { name: 'John Dough', grade: 'B' },
-  { name: 'Jill Do', grade: 'A' },
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
 ];
 
+function enrollInSummerSchool(students){
+  let result = []
+  for (let i = 0; i < students.length; i++){
+    students[i].status = "In summer school"
+  }
+return students
+}
 
-console.log(makeStudentReport(testData));
+
+
+console.log(enrollInSummerSchool(studentData))
+
+
