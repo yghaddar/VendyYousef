16caa52c46ebe15803ba4589d16172b6c4331324
Vendy Prum <vendy@thinkful.com>
2020-02-11 11:41:36 -0800

    adding compute exercise with forEach method

--- maxmin.js
+++ maxmin.js
@@ -1,21 +1,21 @@
  'use strict';
 
 
-// function max(numbers) {
-//   // your code here
-//   let maxNum = -99999;
-//   let i = 0;
-//   while(i < numbers.length){
+function max(numbers) {
+  // your code here
+  let maxNum = -99999;
+  let i = 0;
+  while(i < numbers.length){
 
-//     if(maxNum <= numbers[i]){
-//       maxNum = numbers[i];
-//     }
-//     i++;
-//   }
-//   console.log(maxNum);
-// }
+    if(maxNum <= numbers[i]){
+      maxNum = numbers[i];
+    }
+    i++;
+  }
+  console.log(maxNum);
+}
   
-// console.log(max([1,2,3,4]));
+console.log(max([1,2,3,4]));
 
 
 
@@ -32,3 +32,28 @@ function min(numbers) {
   return minNum
 }
 console.log(min([30,80,20,70]));
+
+
+
+
+/////////////
+//compute average with the forEach method
+
+function average(numbers) {
+  // your code goes here
+  if(numbers === []){
+    return 'input array of numbers'
+  }
+  return numbers.forEach(compute());
+}
+
+
+function compute(num){
+
+  let newValue = 0;
+  for(let i = 0; i < num.length; i++){
+    newValue += num[i];
+  }
+  return Math.floor(newValue / num.length);
+}
+console.log(compute([1,2,3,4]));
