ff422bf17b98d1f83c1ba881f0db1facb91dc5ef
Yghaddar <58191171+yghaddar@users.noreply.github.com>
2020-02-11 13:18:09 -0500

    finished min

--- maxmin.js
+++ maxmin.js
@@ -1,20 +1,21 @@
-'use strict';
+ 'use strict';
 
-function max(numbers) {
-  // your code here
-  let maxNum = -99999;
-  let i = 0;
-  while(i < numbers.length){
 
-    if(maxNum <= numbers[i]){
-      maxNum = numbers[i];
-    }
-    i++;
-  }
-  console.log(maxNum);
-}
+// function max(numbers) {
+//   // your code here
+//   let maxNum = -99999;
+//   let i = 0;
+//   while(i < numbers.length){
+
+//     if(maxNum <= numbers[i]){
+//       maxNum = numbers[i];
+//     }
+//     i++;
+//   }
+//   console.log(maxNum);
+// }
   
-console.log(max([1,2,3,4]));
+// console.log(max([1,2,3,4]));
 
 
 
@@ -23,9 +24,12 @@ function min(numbers) {
   let minNum = numbers[0]
   let i = 0
   while (i < numbers.length){
-    if (numbers[i] > minNum) {
+    if (numbers[i] < minNum) {
       minNum = numbers[i]
     }
     i++
+    return minNum
   }
+  console.log(minNum);
 }
+console.log(min([1,2,3,4]));
