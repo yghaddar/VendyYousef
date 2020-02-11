06a6c025aa3f4dea14b4433d1528669c58c47f02
Vendy Prum <vendy@thinkful.com>
2020-02-11 14:29:36 -0800

    update with 2nd drill finished

--- objectdrills.js
+++ objectdrills.js
@@ -1,10 +1,29 @@
-let loaf = {
-    flour: 300,
-    water: 210,
-    hydration: function() {
-       return  (this.water / this.flour) * 100
-    }
+'use strict';
+//first exercise
+// let loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function() {
+//     return  (this.water / this.flour) * 100;
+//   }
+// };
+
+// console.log(loaf.flour, loaf.water);
+// console.log(loaf.hydration());
+
+//second exercise
+const obj = {
+  foo: 'hi',
+  bar: 'no',
+  fum: 5,
+  quux: 2,
+  spam: 1
+};
+
+
+for(const prop in obj){
+  console.log(`${prop} and ${obj[prop]}`);
 }
 
-console.log(loaf.flour, loaf.water)
-console.log(loaf.hydration())
+//arrays in objects 
+
