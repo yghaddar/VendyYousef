5e41ade310022502abc9ab6627d328627c222298
Yghaddar <58191171+yghaddar@users.noreply.github.com>
2020-02-11 15:23:49 -0500

    added update and delete

--- drillone.js
+++ drillone.js
@@ -1,29 +1,53 @@
 'use strict';
 
-// function createMyObject() {
-//   return obj = {
-//     foo: 'bar',
-//     anwserToUniverse: 42,
-//     "olly olly": 'oxen free',
-//     sayHello: function() {
-//       return 'Hello';
-//     }
-//   };
-// }
-
-//self reference
-function personMaker() {
-  var person = {
-    firstName: 'Paul',
-    lastName: 'Jones',
-    // replace `null` with a function that uses self reference to return
-    // full name
-    fullName: function(){
-      return `${this.firstName} ${this.lastName}`;
-    },
+function createMyObject() {
+  return obj = {
+    foo: 'bar',
+    anwserToUniverse: 42,
+    "olly olly": 'oxen free',
+    sayHello: function() {
+      return 'Hello';
+    }
   };
-  
-  return person;
 }
 
+//self reference
+// function personMaker() {
+//   var person = {
+//     firstName: 'Paul',
+//     lastName: 'Jones',
+//     // replace `null` with a function that uses self reference to return
+//     // full name
+//     fullName: function(){
+//       return `${this.firstName} ${this.lastName}`;
+//     },
+//   };
+  
+//   return person;
+// }
 
+
+
+function updateObject(obj){
+    obj.foo = 'foo'
+    obj.bar = 'bar'
+    obj.bizz = 'bizz'
+    obj.bang = 'bang'
+    return obj
+}
+
+let newObj = {
+    foo: "hi",
+    bar: "bye",
+    bizz: "that should be ok"
+}
+
+console.log(updateObject(newObj))
+
+function keyDeleter(obj) {
+    delete obj.foo
+    delete obj.bar
+    return obj
+}
+
+keyDeleter(newObj)
